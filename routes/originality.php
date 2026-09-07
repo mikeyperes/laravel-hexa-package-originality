3a8c62e87913ae9fca9c7468d0adc296f0e9b5ae
mikeyperes <mikeyperes@users.noreply.github.com>
2026-09-07 23:14:35 +0000

    Require role authorization on Originality routes

--- routes/originality.php
+++ routes/originality.php
@@ -3,7 +3,7 @@
 use hexa_package_originality\Http\Controllers\OriginalityController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web', 'auth'])->group(function () {
+Route::middleware(['web', 'auth', 'locked', 'system_lock', 'two_factor', 'role'])->group(function () {
     Route::get('originality/settings', [OriginalityController::class, 'settings'])->name('originality.settings');
     Route::post('originality/settings', [OriginalityController::class, 'saveSettings'])->name('originality.settings.save');
     Route::post('originality/test', [OriginalityController::class, 'testConnection'])->name('originality.test');
